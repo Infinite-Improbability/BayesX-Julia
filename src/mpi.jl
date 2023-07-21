3b2358be044dd44e6149b685b4672cdb68b0bcd2
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2023-07-21 14:16:58 +1200

    extend logging hack to other log types

--- src/mpi.jl
+++ src/mpi.jl
@@ -6,21 +6,26 @@ MPI.Init()
 const comm = MPI.COMM_WORLD
 
 macro mpidebug(exs...)
-    return :(MPI.Comm_rank(comm) != 1 ? nothing : @debug $(exs...))
+    return :(MPI.Comm_rank(comm) > 0 ? nothing : @debug $(esc.(exs)...))
 end
 
 macro mpiinfo(exs...)
-    return :(MPI.Comm_rank(comm) != 1 ? nothing : @info $(exs...))
+    return :(MPI.Comm_rank(comm) > 0 ? nothing : @info $(esc.(exs)...))
 end
 
 macro mpiwarn(exs...)
-    return :(MPI.Comm_rank(comm) != 1 ? nothing : @warn $(exs...))
+    return :(MPI.Comm_rank(comm) > 0 ? nothing : @warn $(esc.(exs)...))
 end
 
 macro mpierror(exs...)
-    return :(MPI.Comm_rank(comm) != 1 ? nothing : @error $(exs...))
+    return :(MPI.Comm_rank(comm) > 0 ? nothing : @error $(esc.(exs)...))
 end
 
-macro mpirankeddebug(exs...)
-    return :(@debug "[Process $(MPI.Comm_rank(comm))/$(MPI.Comm_size(comm))] $($exs[1] )" $(esc.(exs[2:end])...))
+macro mpirankedinfo(msg, exs...)
+    return :(@info "[Process $(MPI.Comm_rank(comm))/$(MPI.Comm_size(comm))] $($msg)" $(esc.(exs)...))
+end
+
+
+macro mpirankeddebug(msg, exs...)
+    return :(@debug "[Process $(MPI.Comm_rank(comm))/$(MPI.Comm_size(comm))] $($msg)" $(esc.(exs)...))
 end
