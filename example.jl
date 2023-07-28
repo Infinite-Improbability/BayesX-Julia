bc8452ee96dfbf47043a597247de1ff02314dd53
Infinite-Improbability <coxryan@myvuw.ac.nz>
2023-07-28 16:13:48 +1200

    update example script to use env

--- example.jl
+++ example.jl
@@ -1,15 +1,17 @@
 include("src/BayesJ.jl")
 using .BayesJ
 using Unitful, DimensionfulAngles
-using Profile
+using DotEnv
+
+DotEnv.config()
 
 # ENV["JULIA_DEBUG"] = "BayesJ"
 
 data = FITSData(
-    "/home/ryan/data/chandra/4361/manual3/repro/acisf04361_repro_evt2.fits",
-    "/home/ryan/data/chandra/4361/manual3/repro/bg_trimmed_300-7000.fits",
-    "/home/ryan/data/chandra/4361/manual3/repro/specx/specx.arf",
-    "/home/ryan/data/chandra/4361/manual3/repro/specx/specx.rmf",
+    "$(ENV["DEBUG_DATA_PATH"])/acisf04361_repro_evt2.fits",
+    "$(ENV["DEBUG_DATA_PATH"])/4361_blanksky.fits",
+    "$(ENV["DEBUG_DATA_PATH"])/specx/specx.arf",
+    "$(ENV["DEBUG_DATA_PATH"])/specx/specx.rmf",
     0.492u"arcsecondᵃ"
 )
 
