a47d5ebf6179f9bbceff9b09ad2daf6d5cb23e21
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2023-08-07 21:45:52 +1200

    typo fixes

--- examples/debugging.jl
+++ examples/debugging.jl
@@ -64,53 +64,54 @@ replace!(model, NaN => 0.0)
 
 noisy = pois_rand.(model)
 
-# @profview Model_NFW_GNFW(
-#     mass,
-#     fg,
-#     gnfw...,
-#     redshift,
-#     shape,
-#     pixel_size,
-#     em,
-#     exposure_time,128
-#     (0u"arcsecondᵃ", 0u"arcsecondᵃ"),
-#     0
-# )
-# @profview_allocs Model_NFW_GNFW(
-#     mass,
-#     fg,
-#     gnfw...,
-#     redshift,
-#     shape,
-#     pixel_size,
-#     em,
-#     exposure_time,
-#     response,
-#     (0u"arcsecondᵃ", 0u"arcsecondᵃ"),
-#     0
-# )
-# display(
-#     @benchmark Model_NFW_GNFW(
-#         mass,
-#         fg,
-#         gnfw...,
-#         redshift,
-#         shape,
-#         pixel_size,
-#         em,
-#         exposure_time,
-#         response,
-#         (0u"arcsecondᵃ", 0u"arcsecondᵃ"),
-#         0
-#     )
-# )
+@profview Model_NFW_GNFW(
+    mass,
+    fg,
+    gnfw...,
+    redshift,
+    shape,
+    pixel_size,
+    em,
+    exposure_time,
+    response,
+    (0u"arcsecondᵃ", 0u"arcsecondᵃ"),
+    0
+)
+@profview_allocs Model_NFW_GNFW(
+    mass,
+    fg,
+    gnfw...,
+    redshift,
+    shape,
+    pixel_size,
+    em,
+    exposure_time,
+    response,
+    (0u"arcsecondᵃ", 0u"arcsecondᵃ"),
+    0
+)
+display(
+    @benchmark Model_NFW_GNFW(
+        mass,
+        fg,
+        gnfw...,
+        redshift,
+        shape,
+        pixel_size,
+        em,
+        exposure_time,
+        response,
+        (0u"arcsecondᵃ", 0u"arcsecondᵃ"),
+        4
+    )
+)
 
-bg_rate = 8.4e-6u"cm^-2/arcminuteᵃ^2/s";
-avg_eff_area = 250u"cm^2";
-n_channels = size(model, 1);
-bg_count = bg_rate * exposure_time * avg_eff_area *
-           pixel_size^2 / n_channels;
-bg = fill(upreferred(bg_count), size(model));
+# bg_rate = 8.4e-6u"cm^-2/arcminuteᵃ^2/s";
+# avg_eff_area = 250u"cm^2";
+# n_channels = size(model, 1);
+# bg_count = bg_rate * exposure_time * avg_eff_area *
+#            pixel_size^2 / n_channels;
+# bg = fill(upreferred(bg_count), size(model));
 
 # display(
 #     heatmap(
@@ -125,23 +126,23 @@ bg = fill(upreferred(bg_count), size(model));
 #     )
 # )
 
-s = sample(
-    round.(Int, noisy + bg),
-    round.(Int, bg),
-    response,
-    make_cube_transform(
-        UniformPrior(1e14, 1e15),
-        UniformPrior(0.08, 0.2)
-    ),
-    exposure_time,
-    exposure_time,
-    redshift,
-    emission_model=em,
-    pixel_edge_angle=pixel_size,
-    background_rate=bg_rate,
-    average_effective_area=avg_eff_area,
-    center_radius=12
-)
+# s = sample(
+#     round.(Int, noisy + bg),
+#     round.(Int, bg),
+#     response,
+#     make_cube_transform(
+#         UniformPrior(1e14, 1e15),
+#         UniformPrior(0.08, 0.2)
+#     ),
+#     exposure_time,
+#     exposure_time,
+#     redshift,
+#     emission_model=em,
+#     pixel_edge_angle=pixel_size,
+#     background_rate=bg_rate,
+#     average_effective_area=avg_eff_area,
+#     center_radius=12
+# )
 
-posterior = s[2]["posterior"]
-@assert all(posterior["errlo"][1:2] .< [ustrip(mass), fg] .< posterior["errup"][1:2]) display(posterior)
+# posterior = s[2]["posterior"]
+# @assert all(posterior["errlo"][1:2] .< [ustrip(mass), fg] .< posterior["errup"][1:2]) display(posterior)
