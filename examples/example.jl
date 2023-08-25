236bedba9ec8038e18d00ff5c47deb4b056ec7fe
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2023-08-25 14:08:20 +1200

    tng with core exclusion

--- examples/example.jl
+++ examples/example.jl
@@ -5,10 +5,10 @@ using Unitful, DimensionfulAngles
 # ENV["JULIA_DEBUG"] = "BayesJ"
 
 data = FITSData(
-    "data/2203/reproj_deflared_filt_evt.fits",
-    "data/2203/2203_blanksky_deflared_filt_evt.fits",
-    "data/2203/2203_specx.arf",
-    "data/2203/2203_specx.rmf",
+    "data/tng/tng_s67_h11_obs_evt.fits",
+    "data/tng/tng_s67_h11_bg.fits",
+    "data/tng/acisi_aimpt_cy0.arf",
+    "data/tng/acisi_aimpt_cy0.rmf",
     0.492u"arcsecondᵃ"
 )
 
@@ -47,10 +47,10 @@ priors_eiansto = [
     DeltaPrior("c_500_GNFW", 1.177)
 ]
 priors_nfw = [
-    UniformPrior("x0", -150.0, 150.0),
-    UniformPrior("y0", -10.0, 10.0),
-    # DeltaPrior("x0", 0),
-    # DeltaPrior("y0", 0),
+    # UniformPrior("x0", -10.0, 10.0),
+    # UniformPrior("y0", -10.0, 10.0),
+    DeltaPrior("x0", 0),
+    DeltaPrior("y0", 0),
     UniformPrior("MT_200", 1.0e14, 1.0e15),
     NormalPrior("fg_200", 0.13, 0.01),
     DeltaPrior("a", 1.0510),
@@ -61,14 +61,14 @@ priors_nfw = [
 
 sample(
     data,
-    (0.5:0.01:4.0)u"keV",
+    range(0.2u"keV", 4.0u"keV", length=329),
     Model_NFW,
     priors_nfw,
     0.022e22u"cm^-2",
-    0.0569,
-    (3100, 4850),
-    (3685, 4505);
+    0.5,
+    (1900, 2800),
+    (1900, 2800);
     bin_size=10,
-    centre_radius=0,
+    centre_radius=10,
     use_interpolation=false
 )
