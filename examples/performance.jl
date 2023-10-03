a62b67aacea011a4c83d0a9f0b5a8321305f3766
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2023-10-03 16:46:14 +1300

    improve performance script output readability

--- examples/performance.jl
+++ examples/performance.jl
@@ -1,7 +1,8 @@
-using Profile, BenchmarkTools
+using BenchmarkTools
 using Unitful, UnitfulAstro
 using BayesJ
 
+println("######################################################")
 println("PROFILE GENERATION")
 nfw_params = (5.0e14, 0.13, 4.0, 1.0620, 5.4807, 0.3292, 1.156)
 einasto_params = (5.0e14, 0.13, 4.0, 1.0, 1.0620, 5.4807, 0.3292, 1.156)
@@ -9,12 +10,15 @@ v2006_params = (4.705e-3, 0.247e-1, 94.6, 75.83, 0.916, 0.526, 3.607, 4.943, 123
 
 println("Model_NFW")
 display(@benchmark Model_NFW($nfw_params..., z=0.1))
+println("-----------------")
 
 println("\nModel_Einasto")
 display(@benchmark Model_Einasto($einasto_params..., z=0.1))
+println("-----------------")
 
 println("\nModel_Vikhlinin2006")
 display(@benchmark Model_Vikhlinin2006($v2006_params...))
+println("=========================================")
 
 
 println("\n\nPOINT CALCULATIONS")
@@ -27,15 +31,18 @@ println("Temperature")
 display(@benchmark nfw_temperature($rand() * 1u"Mpc"))
 println("Density")
 display(@benchmark nfw_density($rand() * 1u"Mpc"))
+println("-----------------")
 
 println("\nModel_Einasto")
 println("Temperature")
 display(@benchmark einasto_temperature($rand() * 1u"Mpc"))
 println("Density")
 display(@benchmark einasto_density($rand() * 1u"Mpc"))
+println("-----------------")
 
 println("\nModel_Vikhlinin2006")
 println("Temperature")
 display(@benchmark v2006_temperature($rand() * 1u"Mpc"))
 println("Density")
 display(@benchmark v2006_density($rand() * 1u"Mpc"))
+println("=========================================")
