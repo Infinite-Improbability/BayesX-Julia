4213045eefdc85c71723e09783616f2d5199e776
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2024-01-10 17:27:37 +1300

    ic test runs through to end now

--- test/test_internal_consistency.jl
+++ test/test_internal_consistency.jl
@@ -5,13 +5,13 @@ using PoissonRandom
 function test_single_cell_consistency()
     r = 0.5u"kpc"
     T = 2.0u"keV"
-    ρ = 1.0e-12u"g/cm^3"
+    ρ = 1.0e-20u"g/cm^3"
     z = 0.1
     shape = (1, 1)
     pixel_edge_angle = 0.0492u"arcsecondᵃ"
     energy_bins = range(0.7u"keV", 3.0u"keV", length=100)
     exposure_time = 3.0u"s"
-    response_function = rand(Float64, (10, length(energy_bins) - 1)) * 1u"cm^2"
+    response_function = rand(Float64, (50, length(energy_bins) - 1)) * 1u"cm^2"
     centre_radius = 0
     integration_limit = 1.0u"kpc"
 
@@ -43,18 +43,19 @@ function test_single_cell_consistency()
     background_rate = rand()
     background = Array{Int64}(undef, size(observation)...)
     for i in eachindex(background)
-        background[i] = pois_rand(background_rate)
+        background[i] = pois_rand(background_rate) + 1
     end
 
     priors = [
         BayesJ.DeltaPrior("x0", 0.0),
         BayesJ.DeltaPrior("y0", 0.0),
-        BayesJ.UniformPrior("T", 0.0, 10.0),
-        BayesJ.UniformPrior("ρ", 0.0, 10.0)
+        BayesJ.DeltaPrior("r", ustrip(u"Mpc", r)),
+        # BayesJ.UniformPrior("T", 0.0, 10.0),
+        BayesJ.DeltaPrior("T", 2.0),
+        BayesJ.LogUniformPrior("ρ", 1.0e-24, 1.0e-15),
     ]
     prior_transform, param_wrapper = BayesJ.make_cube_transform(priors...)
 
-    model_fixed_r(t::Real, p::Real) = Model_Constant(ustrip(u"Mpc", r), t, p)
     sampler, result, best_fit_observation = BayesJ.sample(
         observation + background,
         background,
@@ -63,23 +64,27 @@ function test_single_cell_consistency()
         exposure_time,
         exposure_time, # TODO: Make different
         z;
-        prior_names=["T", "ρ"],
-        cluster_model=model_fixed_r,
+        prior_names=["ρ"],
+        cluster_model=Model_Constant,
         emission_model=emission_model,
         param_wrapper=param_wrapper,
         pixel_edge_angle=pixel_edge_angle,
         centre_radius=centre_radius,
         log_dir=nothing,
         integration_limit=integration_limit,
-        ultranest_run_args=(max_num_improvement_loops=3, min_num_live_points=100)
+        ultranest_run_args=(
+            max_num_improvement_loops=3,
+            min_num_live_points=100,
+            show_status=false,
+            viz_callback=false
+        )
     )
 
     lower_bound = result["posterior"]["errlo"]
-    upper_bound = result["posterior"]["errhi"]
+    upper_bound = result["posterior"]["errup"]
 
-    @testset "Single Cell IC" begin
-        @test lower_bound[0] < T < upper_bound[0]
-        @test lower_bound[1] < ρ < upper_bound[1]
+    @testset "Single Cell IC (fit ρ)" begin
+        @test lower_bound[1] < ustrip(u"g/cm^3", ρ) < upper_bound[1]
     end
 
 end
