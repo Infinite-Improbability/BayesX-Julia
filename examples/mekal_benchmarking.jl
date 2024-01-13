f93f87ea55e10aa12395786d0e5dc92127eae32b
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2024-01-13 16:18:34 +1300

    more benchmarking

--- examples/mekal_benchmarking.jl
+++ examples/mekal_benchmarking.jl
@@ -1,8 +1,24 @@
 using BayesJ
 using BenchmarkTools
-using Unitful
+using Unitful, UnitfulAstro, DimensionfulAngles
 
 energy_bins = range(0u"keV", 13u"keV", length=1000)
+
+T = 1.0u"keV"
+r = 0.5u"kpc"
+ρ = 1.0e-20u"g/cm^3"
+nH = 1.0e-3u"cm^-3"
+
+z = 0.1
+shape = (100, 100)
+
+pixel_edge_angle = 0.0492u"arcsecondᵃ"
+exposure_time = 3.0u"s"
+response_function = rand(Float64, (100, length(energy_bins) - 1)) * 1u"cm^2"
+centre_radius = 0
+integration_limit = 10.0u"kpc"
+
+temperature, density = Model_Constant(r, T, ρ)
 
 emission_model = BayesJ.prepare_model_mekal(
     2.2e20u"cm^-2",
@@ -11,7 +27,36 @@ emission_model = BayesJ.prepare_model_mekal(
     use_interpolation=false
 )
 
-T = 1.0u"keV"
-nH = 1.0e-3u"cm^-3"
-
 display(@benchmark emission_model($T, $nH))
+
+projected_radius = 0.3u"kpc"
+
+display(
+    @benchmark begin
+        BayesJ.surface_brightness(
+            $projected_radius,
+            $temperature,
+            $density,
+            $z,
+            $integration_limit,
+            $emission_model,
+            $pixel_edge_angle
+        )
+    end
+)
+
+display(@benchmark begin
+    BayesJ.make_observation(
+        temperature,
+        density,
+        z,
+        shape,
+        pixel_edge_angle,
+        emission_model,
+        exposure_time,
+        response_function,
+        (0u"arcsecondᵃ", 0u"arcsecondᵃ"),
+        centre_radius,
+        limit=integration_limit,
+    )
+end)
