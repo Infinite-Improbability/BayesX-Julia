3ad4896c3f15ed1630981259159695210405afb5
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2024-01-19 13:18:11 +1300

    try example with piecewise model

--- examples/tng_s91h57.jl
+++ examples/tng_s91h57.jl
@@ -28,34 +28,20 @@ gas_metals = [
 ]
 abundances = convert_to_anders(gas_metals, gas_metal_fractions)
 
-priors_v2006 = [
-    DeltaPrior("x0", 80.534), # from centre finding fit
-    DeltaPrior("y0", 67.006), # from centering finding fit
-    UniformPrior("n0", 0.1e-3, 40.0e-3),
-    UniformPrior("n02", 0.001e-2, 6.0e-1),
-    DependentLogUniformPrior("rc", "rc2", 600.0),
-    UniformPrior("rc2", 1.0, 100.0),
-    UniformPrior("α", 0.1, 3.0),
-    UniformPrior("β", 0.1, 2.0),
-    UniformPrior("β2", 0.1, 5.0),
-    UniformPrior("ϵ", 0.1, 5.0), # constrain ϵ<5
-    DependentLogUniformPrior("rs", "rc", 1400.0),
-    UniformPrior("T0", 1.0, 20.0),
-    UniformPrior("Tmin/T0", 0.01, 1.0),
-    UniformPrior("rcool", 1.0, 250.0),
-    UniformPrior("acool", 0.1, 10.0),
-    UniformPrior("rt", 0.01, 10.0),
-    UniformPrior("a", -1.0, 1.0),
-    UniformPrior("b", 0.1, 6.0),
-    UniformPrior("c", 0.1, 10.0),
-    LogUniformPrior("d", 1.0e-12, 1.0),
+priors_piecewise = [
+    DeltaPrior("x0", 80.534), DeltaPrior("y0", 67.006), # from centering finding fit
+    DeltaPrior("r0", 0.0), UniformPrior("ρ0", 0.0, 1e-22), UniformPrior("T0", 0.0, 10.0),
+    UniformPrior("r1", 0.0, 1000.0), UniformPrior("ρ1", 0.0, 1e-22), UniformPrior("T1", 0.0, 10.0),
+    DependentUniformPrior("r2", "r1", 1000.0), UniformPrior("ρ2", 0.0, 1e-22), UniformPrior("T2", 0.0, 10.0),
+    DependentUniformPrior("r3", "r2", 1000.0), UniformPrior("ρ3", 0.0, 1e-22), UniformPrior("T3", 0.0, 10.0),
+    DependentUniformPrior("r4", "r3", 1000.0), DeltaPrior("ρ4", 0.0), DeltaPrior("T4", 0.0),
 ]
 
 sample(
     data,
     (0.7u"keV", 7.0u"keV"),
-    Model_Vikhlinin2006,
-    priors_v2006,
+    Model_Piecewise,
+    priors_piecewise,
     0.022e22u"cm^-2",
     0.1,
     (1340, 3400),
