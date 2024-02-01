31ab876b8743b4c59a751500e64b3b487e0ea033
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2024-02-01 15:59:10 +1300

    tweak empty bg handling

--- src/run.jl
+++ src/run.jl
@@ -71,16 +71,19 @@ function prepare_background(
 )::NTuple{2,Vector{Float64}}
     # implicitly includes average effective area and pixel edge angle
     bg_count_rate = [mean(@view observed_background[i, :, :]) for i in axes(observed_background, 1)] ./ bg_exposure_time
-    number_of_zeros_in_bg = count(i -> i == 0u"s^-1", bg_count_rate)
-    if number_of_zeros_in_bg > 0
-        @mpiwarn "Background count has areas of zero counts, replacing with fallback value" number_of_zeros_in_bg
-        replace!(bg_count_rate, 0u"s^-1" => 0.1e-6 / bg_exposure_time)
+    zero_channels = count(i -> i == 0u"s^-1", bg_count_rate)
+    total_channels = size(observed_background, 1)
+    fallback_count_rate = 1.0e-7 / bg_exposure_time
+    if zero_channels > 0
+        @mpiwarn "Some energy channels have zero background counts. Replacing with fallback value" zero_channels total_channels fallback_count_rate
+        replace!(bg_count_rate, 0u"s^-1" => fallback_count_rate)
     end
 
     @assert all(i -> i > 0u"s^-1", bg_count_rate)
     @mpidebug "Background rate estimated" bg_count_rate
 
     # vector of background as a function of energy
+    # counts per pixel per exposure time
     predicted_obs_bg = bg_count_rate * obs_exposure_time # Used for adding background to observations
     predicted_bg_bg = bg_count_rate * bg_exposure_time # Used for log likelihood
 
