600e94da3960740dab1d0784f6e8caab2fd7752d
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2024-02-16 18:20:19 +1300

    fix einasto pei integrand

--- src/cluster_models/model_einasto.jl
+++ src/cluster_models/model_einasto.jl
@@ -79,7 +79,7 @@ function Model_Einasto(
     function pei_integrand(r, params)
         rs, n, rp, α, β, γ = params
         r^3 * (β * (r / rp)^α + γ) /
-        safe_lower_gamma(3 / n, 2 / n * (r / rs)^n) /
+        safe_lower_gamma(3n, 2n * (r / rs)^(1 / n)) /
         (r / rp)^γ / (1 + (r / rp)^α)^((α + β - γ) / α)
     end
 
