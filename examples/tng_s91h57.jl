5d31f1cea14467c0008289de28bfba03eabe1432
Infinite-Improbability <ryan.cox@vuw.ac.nz>
2024-02-19 14:57:43 +1300

    chanda s91h57 centre finding

--- examples/tng_s91h57.jl
+++ examples/tng_s91h57.jl
@@ -5,16 +5,16 @@ using Cosmology
 include("metallicity.jl")
 
 # Load data
-data = FITSData(
-    "../data/tng_s91_h57_4/y/lynx_hdxi_obs_evts.fits",
-    "../data/tng_s91_h57_4/bg/lynx_hdxi_bg_evts.fits",
-    "../data/tng_s91_h57_4/response_files/lynx_hdxi/xrs_hdxi_3x10.arf",
-    "../data/tng_s91_h57_4/response_files/lynx_hdxi/xrs_hdxi.rmf",
-    0.33u"arcsecondᵃ"
+const data = FITSData(
+    "../data/tng_s91_h57_4/y/chandra_acisi_cy0_obs_evts.fits",
+    "../data/tng_s91_h57_4/bg/chandra_acisi_cy0_bg_evts.fits",
+    "../data/tng_s91_h57_4/response_files/acisi/acisi_aimpt_cy0.arf",
+    "../data/tng_s91_h57_4/response_files/acisi/acisi_aimpt_cy0.rmf",
+    0.492u"arcsecondᵃ"
 )
 
 # Load abundances from tng metadata
-gas_metal_fractions = [
+const gas_metal_fractions = [
     0.7543419003486633,
     0.24289754033088684,
     0.0002910351031459868,
@@ -26,47 +26,38 @@ gas_metal_fractions = [
     0.00016363433678634465,
     8.585586328990757e-05
 ]
-gas_metals = [
+const gas_metals = [
     "H", "He", "C", "N", "O", "Ne", "Mg", "Si", "Fe", "Other"
 ]
-abundances = convert_to_anders(gas_metals, gas_metal_fractions)
+const abundances = convert_to_anders(gas_metals, gas_metal_fractions)
 
-priors_nfw = [
+const c200 = 6.077
+const r200 = 1338.216892862
+const rs = r200 / c200
+const r500 = 885.727387822
+const c500 = r500 / rs
+
+const priors_nfw = [
     NormalPrior("x0", 0.0, 25.0), NormalPrior("y0", 0.0, 25.0),
-    UniformPrior("MT_200", 1.0e14, 4.0e14),
-    NormalPrior("fg_200", 0.13, 0.01),
-    DeltaPrior("c_200", 6.077),
-    NormalPrior("a", 1.0510, 0.06),
-    NormalPrior("b", 5.4905, 1.0),
-    NormalPrior("c", 0.3081, 0.02),
-    NormalPrior("c_500_GNFW", 1.177, 0.02)
+    DeltaPrior("MT_500", 20445.219658806e10),
+    DeltaPrior("fg_500", 0.13),
+    DeltaPrior("c_500", c500),
+    DeltaPrior("a", 1.0510),
+    DeltaPrior("b", 5.4905),
+    DeltaPrior("c", 0.3081),
+    DeltaPrior("c_500_GNFW", 1.177)
 ]
-
-priors_einasto = [
-    NormalPrior("x0", 0.0, 25.0), NormalPrior("y0", 0.0, 25.0),
-    UniformPrior("MT_200", 1.0e14, 4.0e14),
-    NormalPrior("fg_200", 0.13, 0.01),
-    DeltaPrior("c_200", 6.077),
-    UniformPrior("n", 0.1, 10.0),
-    NormalPrior("a", 1.0510, 0.06),
-    NormalPrior("b", 5.4905, 1.0),
-    NormalPrior("c", 0.3081, 0.02),
-    NormalPrior("c_500_GNFW", 1.177, 0.02)
-]
-
-model_nfw(args...; kwargs...) = Model_NFW(args...; Δ=200, kwargs...)
-model_einasto(args...; kwargs...) = Model_Einasto(args...; Δ=200, kwargs...)
 
 sample(
     data,
     (0.7u"keV", 7.0u"keV"),
-    model_nfw,
+    Model_NFW,
     priors_nfw,
     0.022e22u"cm^-2",
     0.1,
-    (2048, 6144),
-    (2048, 6144);
-    bin_size=128,
+    (1340, 3340),
+    (1370, 3400);
+    bin_size=32,
     centre_radius=0,
     abundances=abundances,
     use_stepsampler=false,
