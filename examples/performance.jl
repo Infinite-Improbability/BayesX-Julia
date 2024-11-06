604465cc7b8c137794aebd46afc1274605c8ff35
Infinite-Improbability <65998330+Infinite-Improbability@users.noreply.github.com>
2024-11-06 14:01:48 +1300

    performance test script

--- examples/performance.jl
+++ examples/performance.jl
@@ -1,48 +1,27 @@
 using BenchmarkTools
-using Unitful, UnitfulAstro
+using Unitful, UnitfulAstro, DimensionfulAngles
 using BayesJ
+using LinearAlgebra: I
 
-println("######################################################")
-println("PROFILE GENERATION")
-nfw_params = (5.0e14, 0.13, 4.0, 1.0620, 5.4807, 0.3292, 1.156)
-einasto_params = (5.0e14, 0.13, 4.0, 1.0, 1.0620, 5.4807, 0.3292, 1.156)
-v2006_params = (4.705e-3, 0.247e-1, 94.6, 75.83, 0.916, 0.526, 3.607, 4.943, 1239.9, 3.61, 0.27, 57, 3.88, 1.42, 0.12, 5.00, 10.0)
+redshift = 0.1
+nfw = NFWModel(3e14u"Msun", 0.13, 4.0, 1.0620, 5.4807, 0.3292, 1.156, z=redshift)
+energy_range = range(0.1u"keV", 13u"keV", length=100)
+mekal = prepare_model_mekal(
+    0.022e22u"cm^-2",
+    energy_range,
+    redshift
+)
+response_function = Matrix{Float64}(I, length(energy_range) - 1, length(energy_range) - 1) * 1u"cm^2"
 
-println("Model_NFW")
-display(@benchmark Model_NFW($nfw_params..., z=0.1))
-println("-----------------")
-
-println("\nModel_Einasto")
-display(@benchmark Model_Einasto($einasto_params..., z=0.1))
-println("-----------------")
-
-println("\nModel_Vikhlinin2006")
-display(@benchmark Model_Vikhlinin2006($v2006_params...))
-println("=========================================")
-
-
-println("\n\nPOINT CALCULATIONS")
-nfw_temperature, nfw_density = Model_NFW(nfw_params..., z=0.1)
-einasto_temperature, einasto_density = Model_Einasto(einasto_params..., z=0.1)
-v2006_temperature, v2006_density = Model_Vikhlinin2006(v2006_params...)
-
-println("Model_NFW")
-println("Temperature")
-display(@benchmark nfw_temperature($rand() * 1u"Mpc"))
-println("Density")
-display(@benchmark nfw_density($rand() * 1u"Mpc"))
-println("-----------------")
-
-println("\nModel_Einasto")
-println("Temperature")
-display(@benchmark einasto_temperature($rand() * 1u"Mpc"))
-println("Density")
-display(@benchmark einasto_density($rand() * 1u"Mpc"))
-println("-----------------")
-
-println("\nModel_Vikhlinin2006")
-println("Temperature")
-display(@benchmark v2006_temperature($rand() * 1u"Mpc"))
-println("Density")
-display(@benchmark v2006_density($rand() * 1u"Mpc"))
-println("=========================================")
+b1 = @benchmark (BayesJ.make_observation(
+    nfw,
+    redshift,
+    (30, 30),
+    0.492u"arcsecondᵃ",
+    mekal,
+    3000u"s",
+    response_function,
+    (0u"arcsecondᵃ", 0u"arcsecondᵃ"),
+    1,
+));
+display(b1);
