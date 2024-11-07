3cfebe0eae616f2e66b22ba68505f14ab9d7dbb7
Infinite-Improbability <65998330+Infinite-Improbability@users.noreply.github.com>
2024-11-07 17:07:41 +1300

    fix model data downloading

--- src/mekal.jl
+++ src/mekal.jl
@@ -15,33 +15,9 @@ using LibXSPEC_jll
 export prepare_model_mekal, prepare_model_mekal_interpolation
 
 """Mekal model using SpectralFitting.jl framework. Kept around so we can borrow the model data downloading functions."""
-@xspecmodel :C_mekal struct XS_Mekal{T,F} <: SpectralFitting.AbstractSpectralModel{T,SpectralFitting.Additive}
-    "Normalisation"
-    K::T
-    "Plasma Temperature"
-    t::T
-    "Hydrogen Density"
-    ρ::T
-    "Metal Abundances"
-    a::T
-    "Redshift"
-    z::T
-    "Switch"
-    s::Int
-end
-function XS_Mekal(;
-    K=FitParam(1.0), # these values were picked arbitarily
-    t=FitParam(8.0),
-    ρ=FitParam(10.0),
-    a=FitParam(1.0),
-    z=FitParam(0.1),
-    s=0
-)
-    XS_Mekal{typeof(K),SpectralFitting.FreeParameters{(:K, :t, :ρ)}}(
-        K, t, ρ, a, z, s
-    )
-end
-SpectralFitting.register_model_data(XS_Mekal, "mekal1.dat", "mekal2.dat", "mekal3.dat", "mekal4.dat", "mekal5.dat", "mekal6.dat")
+@xspecmodel :C_mekal struct XS_Mekal{T,F} <: SpectralFitting.AbstractSpectralModel{T,SpectralFitting.Additive} end
+function XS_Mekal() end
+SpectralFitting.register_model_data(XS_Mekal, "mekal1.dat", "mekal2.dat", "mekal3.dat", "mekal4.dat", "mekal5.dat", "mekal6.dat"; root=XSPECModels.LIBXSPEC_STORAGE_PATH)
 
 
 const mekal_factor = 3.03103f-9 / 2.53325f-3
